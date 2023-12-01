15ab3d6f8aab6daa3783cd089ceb46260a3c0538
Gabo <gabo.gh12398@gmail.com>
2023-12-01 08:52:06 -0300

    Reto #1 Completo JavaScript

--- Retos/Reto #1 - EL LENGUAJE HACKER [Fácil]/javascript/GaboDev23.js
+++ Retos/Reto #1 - EL LENGUAJE HACKER [Fácil]/javascript/GaboDev23.js
@@ -1,3 +1,14 @@
 let texto = prompt('Escribe un texto');
+let textoNuevo = '';
 
-let letras = {a:'4', A:'4', b:'13', B:'13', c:'[', C:'[', d:')', D:')', e:'3', E:'3', f:'|=', F:'|=', g:'&', G:'&', h:'#', H:'#', i:'1', I:'1', j:',_|', J:',_|', k:'>|', K:'>|', l:'£', L:'£', m:'JVI', M:'JVI', n:'^/', N:'^/', o:'0', O:'0', p:'|*', P:'|*'}
+let letras = {a:'4', A:'4', b:'13', B:'13', c:'[', C:'[', d:')', D:')', e:'3', E:'3', f:'|=', F:'|=', g:'&', G:'&', h:'#', H:'#', i:'1', I:'1', j:',_|', J:',_|', k:'>|', K:'>|', l:'£', L:'£', m:'JVI', M:'JVI', n:'^/', N:'^/', o:'0', O:'0', p:'|*', P:'|*', q:'(_,)', Q:'(_,)', r:'I2', R:'I2', s:'5', S:'5', t:'7', T:'7', u:'(_)', U:'(_)', v:'|/', V:'|/', w:'VV', W:'VV', x:'><', X:'><', y:'j', Y:'j', z:'2', Z:'2', 0:'o', 1:'L', 2:'Z', 3:'E', 4:'A', 5:'S', 6:'b', 7:'T', 8:'B', 9:'g'};
+
+for (const caracter of texto) {
+    for (const letra in letras) {
+        if (caracter.includes(letra)) {
+            textoNuevo += letras[letra];
+        }
+    }
+}
+
+console.log(textoNuevo);
