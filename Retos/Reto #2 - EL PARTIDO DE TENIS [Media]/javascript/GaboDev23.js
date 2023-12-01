e62dc89a7e751307274e83e001bd9bb97c98e3f7
Gabo <gabo.gh12398@gmail.com>
2023-12-01 15:57:20 -0300

    Reto #2 Completo JavaScript

--- Retos/Reto #2 - EL PARTIDO DE TENIS [Media]/javascript/GaboDev23.js
+++ Retos/Reto #2 - EL PARTIDO DE TENIS [Media]/javascript/GaboDev23.js
@@ -1,21 +1,33 @@
 const partido = (p1, p2) => {
-    if (p1 >= 40 && p2 >= 40 && p1 == p2) console.log('Deuce');
-    else if (p1 >= 40 && p2 >= 40 && p1 > p2 && p1 - p2 == 5) console.log('Jugador 1 lleva la ventaja');
-    else if (p1 >= 40 && p2 >= 40 && p2 > p1 && p2 - p1 == 5) console.log('Jugador 2 lleva la ventaja');
-    else if ((p1 >= 40 || p2 >= 40) && p1 > p2 && p1 - p2 >= 10 && p2 < 30) console.log('Jugador 1 gana');
-    else if ((p1 >= 40 || p2 >= 40) && p2 > p1 && p2 - p1 >= 10 && p1 < 30) console.log('Jugador 2 gana');    
-    else if (p1 == 0) {
+    if (p1 == 0) {
         p1 = 'Love';
         console.log(`${p1} - ${p2}`);
-    }
-    else if (p2 == 0) {
+    } else if (p2 == 0) {
         p2 = 'Love';
         console.log(`${p1} - ${p2}`);
-    } else console.log(`${p1} - ${p2}`);
+    } else if ((p1 >= 40 && p2 >= 40) && (p1 == p2)) console.log('Deuce');
+    else if ((p1 >= 40 && p2 >= 40) && (p1 - p2 == 5)) console.log('Jugador 1 está en ventaja'); 
+    else if ((p1 >= 40 && p2 >= 40) && (p2 - p1 == 5)) console.log('Jugador 2 está en ventaja');
+    else if ((p1 >= 40 && p2 >= 40) && (p1 - p2 >= 10)) console.log('Jugador 1 ha ganado'); 
+    else if ((p1 >= 40 && p2 >= 40) && (p2 - p1 >= 1)) console.log('Jugador 2 ha ganado');
+    else if (p1 == 40 && p2 < 30) console.log('Jugador 1 ha ganado');
+    else if (p2 == 40 && p1 < 30) console.log('Jugador 2 ha ganado');
+    else console.log(`${p1} - ${p2}`);
+
 }
 
 const termina = (p1, p2) => {
-    return (p1 >= 40 || p2 >= 40) && ((p1 - p2 >= 10) || (p1 == 40 && p2 < 30) || (p2 - p1 >= 10) || (p2 == 40 && p1 < 30));
+    if ((p1 >= 40 && p2 >= 40) && (p1 - p2 >= 10)) return true; 
+    else if ((p1 >= 40 && p2 >= 40) && (p2 - p1 >= 10)) return true; 
+    else if (p1 == 40 && p2 < 30) {
+        console.log('Jugador 1 ha ganado');
+        return true;
+    }
+    else if (p2 == 40 && p1 < 30) {
+        console.log('Jugador 2 ha ganado');
+        return true;
+    }
+    return false;
 }
 
 let finPartido = false;
